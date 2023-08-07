e3985372bcb8936399fc845fa73244d12b61ff82
Dare Tunmise <daretunmise@yahoo.com>
2023-08-07 09:43:41 +0100

    first commit to fictional university

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php  
+    $names = array("dare", "bolanle", "laura", "tunmise", "fatima", "timothy", "segun", "jide", "caspian");
+
+    $count = 0;
+
+    while($count < count($names)){
+        echo "<li>hi, everyone, my name is $names[$count]</li>";
+        $count++;
+    }
+     
+?>
+
